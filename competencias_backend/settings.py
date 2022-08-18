e768530eeee38d99dd1dd8f8574acc0d3655d9ef
chico <chico.zimcolor@gmail.com>
2022-08-18 15:32:17 -0300

    chave secreta do Django agora está no .env.dev

--- competencias_backend/settings.py
+++ competencias_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-((cceir@m2vm2-x$agp6*amlf)m+s0gnj2#wzcgu1wt-#o4%i$"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
