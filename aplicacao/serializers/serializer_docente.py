aa97fe8c14c766eb2e0b1bb6a1ac11045c3385d9
chico <chico.zimcolor@gmail.com>
2022-08-24 11:19:22 -0300

    serializers antigos apagados

--- aplicacao/serializers/serializer_docente.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-
-class SerializerDocente(serializers.Serializer):
-    id = serializers.UUIDField()
-    nome = serializers.CharField(max_length=200)
--- aplicacao/serializers/serializer_docente_sem_id.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-
-class SerializerDocenteSemID(serializers.Serializer):
-    nome = serializers.CharField(max_length=200)
