2f6ecf623930067d41df235442214fcd41538ad1
chico <chico.zimcolor@gmail.com>
2022-08-24 14:41:52 -0300

    Docente refatorado

--- dominio/entidades/docente.py
+++ dominio/entidades/docente.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from uuid import UUID, uuid4
 
 from dominio.objetos_de_valor import NomeDeDocente, IdDeDocente
@@ -9,7 +9,7 @@ from dominio.objetos_de_valor import NomeDeDocente, IdDeDocente
 @dataclass(frozen=True)
 class Docente:
     nome: NomeDeDocente
-    id: IdDeDocente = field(default_factory=IdDeDocente)
+    id: IdDeDocente
 
     @classmethod
     def construir(cls, nome: str, id_: UUID = uuid4()) -> Docente:
--- testes/unitarios/dominio/entidades/test_docente.py
+++ testes/unitarios/dominio/entidades/test_docente.py
@@ -1,26 +1,10 @@
 from unittest import TestCase
 
 from dominio.entidades import Docente
-from dominio.objetos_de_valor import IdDeDocente, NomeDeDocente
-from testes.fabricas.dominio.entidades import FabricaTesteDocente
 from testes.fabricas.dominio.objetos_de_valor import FabricaTesteIdDeDocente, FabricaTesteNomeDeDocente
 
 
 class TestDocente(TestCase):
-    def test_init_QUANDO_id_informado_ENTAO_atribui_id(self) -> None:
-        id_de_docente: IdDeDocente = FabricaTesteIdDeDocente.build()
-
-        docente: Docente = FabricaTesteDocente.build(id=id_de_docente)
-
-        self.assertEqual(docente.id, id_de_docente)
-
-    def test_init_QUANDO_id_nao_informado_ENTAO_atribui_id_gerado(self) -> None:
-        nome_de_docente: NomeDeDocente = FabricaTesteNomeDeDocente.build()
-
-        docente = Docente(nome=nome_de_docente)
-
-        self.assertIsInstance(docente.id, IdDeDocente)
-
     def test_construir_QUANDO_id_informado_ENTAO_atribui_id(self) -> None:
         nome = FabricaTesteNomeDeDocente.build().valor
         id_ = FabricaTesteIdDeDocente.build().valor
