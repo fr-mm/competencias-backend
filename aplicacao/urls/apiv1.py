d574a7d625eac052bdb48a626cd41a6e70e6d826
chico <chico.zimcolor@gmail.com>
2022-08-25 13:41:25 -0300

    adiconada / no final da url

--- aplicacao/urls/apiv1.py
+++ aplicacao/urls/apiv1.py
@@ -3,5 +3,6 @@ from django.urls import path
 from aplicacao.views import DocentesView
 
 urlpatterns = [
-    path('docentes', DocentesView.as_view(), name='docentes'),
+    path('docentes/', DocentesView.as_view(), name='docentes'),
+    path('docentes/<uuid:id_de_docente>')
 ]
