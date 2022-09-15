d7ca72a0f8dd155416d768a7a7919982efb7fc75
chico <chico.zimcolor@gmail.com>
2022-09-15 14:17:31 -0300

    aplicacao.apps limpo

--- aplicacao/apps.py
+++ aplicacao/apps.py
@@ -1,13 +1,6 @@
 from django.apps import AppConfig
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
 
 
 class AplicacaoConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "aplicacao"
-    label = 'aplicacao'
-
-    def ready(self):
-        ModeloUsuario = self.get_model('ModeloUsuario')
-        pre_save.connect(receiver, sender='aplicacao.ModeloUsuario')
